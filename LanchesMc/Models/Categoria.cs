1d5d574f33fe0d2a5378457d1c8fbfb8556d647c
Francisco Amaral <francisco.amaral@arquiconsult.com>
2022-04-03 00:44:17 +0100

    Add project files.

---
+++ LanchesMc/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace LanchesMc.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
