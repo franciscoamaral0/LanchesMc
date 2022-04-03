901b560cfb446345004b8355d85a0aa01a6aaf98
Francisco Amaral <francisco.amaral@arquiconsult.com>
2022-04-03 02:30:35 +0100

    Create migrations/updateDB whit migrations, generating tables in SQL

--- LanchesMc/Models/Categoria.cs
+++ LanchesMc/Models/Categoria.cs
@@ -1,11 +1,22 @@
-﻿namespace LanchesMc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMc.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
-        public string CategoriaNome { get; set; }
-        public string Descricao { get; set; }
+        [StringLength(100, ErrorMessage ="O Tamanho máximo é de 100 caracteres.")]
+        [Required(ErrorMessage ="Informe a categoria")]
+        [Display(Name ="Nome")]
 
+        public string CategoriaNome { get; set; }
+        [StringLength(200, ErrorMessage = "O Tamanho máximo é de 200 caracteres.")]
+        [Required(ErrorMessage = "Informe a descricao da categoria")]
+        [Display(Name = "Descricao")]
+        public string Descricao { get; set; }
         public List<Lanche> Lanches { get; set; }
 
     }
