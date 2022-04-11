8ba648d96587e4a293d4379279b5747746854041
Francisco Amaral <francisco.amaral@arquiconsult.com>
2022-04-11 10:45:21 +0100

    Create view lanche, controller and view models whit lanches and category.

---
+++ LanchesMc/Controllers/LancheController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesMc.Repositories.Interfaces;
+using LanchesMc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMc.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lanchesListViewModel);
+
+        }
+    }
+}
---
+++ LanchesMc/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMc.Models;
+
+namespace LanchesMc.ViewModels
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+
+        public string CategoriaAtual { get; set; }
+    }
+}
