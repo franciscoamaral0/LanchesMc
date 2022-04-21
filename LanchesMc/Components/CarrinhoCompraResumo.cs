926cc480960e08de7211802b23a6fff26b9e01de
Francisco Amaral <francisco.amaral@arquiconsult.com>
2022-04-21 15:21:59 +0100

    Criado carrinho na navbar com a quantidade de itens, taghelper para email e controller/view para envio de email

---
+++ LanchesMc/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,28 @@
+﻿using LanchesMc.Models;
+using LanchesMc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMc.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        public readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+           
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVm = new CarrinhoCompraViewModel();
+            carrinhoCompraVm.CarrinhoCompra = _carrinhoCompra;
+            carrinhoCompraVm.CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal();
+            return View(carrinhoCompraVm);
+        }
+    }
+}
